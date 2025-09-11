c371cf6d0f6a94fa0479db21e2b93f7d140b1610
isaiah0311 <isaiahl0311@gmail.com>
2025-09-11 15:15:57 +0700

    Add letter to points conversions

--- src/main.c
+++ src/main.c
@@ -13,7 +13,29 @@
 #define MAX_CHAR_COUNT 17
 
 /**
- * Entry point of the project.
+ * Converts a letter to its point value.
+ *
+ * \param[in] letter Letter.
+ * \return Point value.
+ */
+int letter_to_points(char letter) {
+    static int points[] = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10,
+        1, 1, 1, 1, 4, 4, 8, 4, 10 };
+
+    int value = 0;
+    if (letter >= 'A' && letter <= 'Z') {
+        value = points[letter - 'A'];
+    } else if (letter >= 'a' && letter <= 'z') {
+        value = points[letter - 'a'];
+    } else {
+        fprintf(stderr, "[WARN] Unrecognized character: %c\n", letter);
+    }
+
+    return value;
+}
+
+/**
+ * Reads in a dictionary file and coverts each word into its point value.
  *
  * \return Exit code
  */
@@ -30,9 +52,29 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
+    int word_count = 0;
     char word[MAX_CHAR_COUNT];
+
     while (fgets(word, MAX_CHAR_COUNT, dictionary)) {
-        printf("%s", word);
+        ++word_count;
+
+        const size_t position = strcspn(word, "\r\n");
+        if (position) {
+            word[position] = '\0';
+        } else {
+            fprintf(
+                stderr, "[WARN] Failed to read word on line %d.\n", word_count);
+            continue;
+        }
+
+        const size_t length = strlen(word);
+
+        int points = 0;
+        for (size_t i = 0; i < length; ++i) {
+            points += letter_to_points(word[i]);
+        }
+
+        printf("%s (%d)\n", word, points);
     }
 
     fclose(dictionary);
